f22250c76bd6340d53c9947afec558f56255656c
felipetodev <fe.ossandon.u@gmail.com>
2024-01-15 18:56:24 -0300

    feat: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home () {
   return (
-    <main>
-      Hello Mooonto
+    <main className='h-screen flex flex-col justify-center items-center gap-10 bg-gray-500'>
+      <img className='fill-black' src="mooonto.svg" alt="mooonto-logo" />
+      <Button>
+        Submit
+      </Button>
     </main>
   )
 }
