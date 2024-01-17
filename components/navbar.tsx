c6a449dc31a150919f0b75b76569105554cbf562
felipetodev <fe.ossandon.u@gmail.com>
2024-01-17 13:20:38 -0300

    feat: add nav navigation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 import CurrencySelector from '@/components/currency-selector'
 
 function NavBar () {
   return (
     <nav className='flex items-center justify-between bg-[#FF3B16] px-16 h-16'>
-      <img className='fill-black' src="mooonto.svg" alt="mooonto-logo" />
+      <Link href='/'>
+        <img className='fill-black' src="mooonto.svg" alt="mooonto-logo" />
+      </ Link>
       <ul className='flex items-center gap-x-4'>
         <li>
-          <a href='#' className="text-md text-[#F7F4F0] font-semibold hover:underline">
+          <a href='#calcula' className="text-md text-[#F7F4F0] font-semibold hover:underline">
             Calcula tu precio
           </a>
         </li>
