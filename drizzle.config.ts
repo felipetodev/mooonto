7d3fb08c621bc63e3942444297ce1ab055c7facc
felipetodev <fe.ossandon.u@gmail.com>
2025-06-07 21:27:40 -0400

    feat: add shadcn components

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+
+import type { Config } from "drizzle-kit";
+
+export default defineConfig({
+	schema: "./db/schema.ts",
+	out: "./migrations",
+	dialect: "turso",
+	dbCredentials: {
+		url: process.env.TURSO_CONNECTION_URL!,
+		authToken: process.env.TURSO_AUTH_TOKEN!,
+	},
+}) satisfies Config;
